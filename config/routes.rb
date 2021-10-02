6db9ae898b95e8d1c31eb9acdf6c43365fcebb84
Enrico Gallus <enrico.gallus@gmail.com>
2021-10-03 08:21:28 +0900

    adding dhtmlxscheduler to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root :to => 'home#index'
+  get "data", :to => "event#get", :as => "data"
+  post "data(/:id)", :to => "event#add"
+  put "data/:id", :to => "event#update"
+  delete "data/:id", :to => "event#delete"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
